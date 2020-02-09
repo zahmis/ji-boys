fdf912061867a924593af786dd9ded9f879803e7
EC2 Default User <ec2-user@ip-172-31-35-248.ap-northeast-1.compute.internal>
2020-02-09 17:35:39 +0900

    静的ページ完成

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "自衛隊に入隊したいけど不安な君へ"
+  end 
+  
+  test "should get root" do 
+    get static_pages_home_url
+    assert_response :success
+  end 
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
