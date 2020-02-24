42f9488b3bfc244da8e69b5cea9c9745839f5193
sizma <meda829ratteru86@gmail.com>
2020-02-24 17:52:37 +0900

    記事投稿機能プロトタイプ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:content, :image)
+      params.require(:post).permit(:content, :image, :title)
     end
 
     def correct_user
