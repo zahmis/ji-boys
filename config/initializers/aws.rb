6b71f10498f62a0e8f56818ac9a7ecae8be25548
sizma <meda829ratteru86@gmail.com>
2020-04-15 15:29:59 +0900

    aws 軽微な修正

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,5 +2,5 @@ ActionMailer::Base.add_delivery_method :ses,
   AWS::SES::Base,
   access_key_id:       ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key:   ENV['AWS_SECRET_ACCESS_KEY'],
-  server:              'email.us-west-2.amazonaws.com'
+  server:              'email.us-west-2.amazonaws.com',
   region:              'us-west-2'
